7668e2960de90e7583ad4230c4453680bff9f521
Ahmed Ammar <ahmed29329@gmail.com>
2022-12-05 18:39:38 +0100

    Initial Commit

---
+++ coolshot/coolshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  coolshotApp.swift
+//  coolshot
+//
+//  Created by Ahmed on 05.12.22.
+//
+
+import SwiftUI
+
+@main
+struct coolshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
