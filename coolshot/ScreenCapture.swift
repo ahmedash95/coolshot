884ce1c675012740d3b4702cbe49b3480736efb4
Ahmed Ammar <ahmed29329@gmail.com>
2022-12-11 01:52:37 +0100

    WIP

---
+++ coolshot/ScreenCapture.swift
@@ -0,0 +1,34 @@
+//
+//  ScreenCapture.swift
+//  ScreenCapture
+//
+//  Created by Jack P. on 11/12/2015.
+//  Copyright © 2015 Jack P. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+import ImageIO
+import UniformTypeIdentifiers
+import AppKit
+
+class ScreenCapture {
+    func screenshotWindowAndSuccess() -> Bool {
+        let task = Process()
+        task.launchPath = "/usr/sbin/screencapture"
+        task.arguments = ["-cir"]
+        task.launch()
+        task.waitUntilExit()
+        let status = task.terminationStatus
+        return status == 0
+    }
+
+    func getImageFromPasteboard() -> NSImage {
+        let pasteboard = NSPasteboard.general
+        guard pasteboard.canReadItem(withDataConformingToTypes:
+            NSImage.imageTypes) else { return NSImage() }
+        guard let image = NSImage(pasteboard: pasteboard) else { return
+            NSImage() }
+        return image
+    }
+}
--- coolshot/coolshotApp.swift
+++ coolshot/coolshotApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import Security
 
 @main
 struct coolshotApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        print("Launched")
     }
 }
